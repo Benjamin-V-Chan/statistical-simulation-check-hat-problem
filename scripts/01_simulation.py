2c19755500581551a6dcef5c03824afd88f19b1d
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-29 20:54:50 -0800

    01 simulation script outline

---
+++ scripts/01_simulation.py
@@ -0,0 +1,14 @@
+# Import necessary modules
+# Define function simulate_check_hat(N: int, num_people: int) -> list
+#   Initialize a list to store results
+#   Loop N times:
+#       Generate a list of hats labeled from 1 to num_people
+#       Randomly shuffle the hats
+#       Count how many people get their own hat
+#       Store the count in results list
+#   Return results list
+#
+# If run as main:
+#   Define number of people and number of trials
+#   Run simulation function
+#   Save results to CSV in outputs/
