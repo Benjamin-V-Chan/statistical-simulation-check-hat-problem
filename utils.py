a1a600987d62c460cb60315a1622570320cc7fcd
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-29 20:57:00 -0800

    utils script outline

---
+++ utils.py
@@ -0,0 +1,6 @@
+# Import necessary modules
+# Define function save_to_csv(data: list, file_path: str)
+#   Save list data to CSV
+#
+# Define function load_from_csv(file_path: str) -> list
+#   Load CSV into a list and return
